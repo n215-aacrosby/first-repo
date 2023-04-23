377d3a77211b1550e3c49623ab89abb1522328c2
AaCrosby23 <aaronitcrosby@gmail.com>
2023-04-23 01:24:25 -0400

    Read Description
    - The entire site is able to load content into the app div
    - I messed up the Navigation so I will need to fix that
          - The append and remove is the
          solution but where it's at right now
          is broken

--- final-project/app/app.js
+++ final-project/app/app.js
@@ -1,22 +1,5 @@
 import * as Model from "../data/data.js"
 
-// function initListeners(){
-
-//     $("nav a").on("click", function(e){
-//         btnID = e.currentTarget.id;
-//         pageContentID = btnID + "Content";
-//         console.log(eval(pageContentID));
-
-//         changePageContent(pageContentID);
-//     })
-
-// }
-
-function changePageContent(pageContentVarName){
-    $("#app").html(eval(pageContentVarName));
-
-}
-
 function initURLListener(){
   $(window).on("hashchange", Model.changeRoute);
   Model.changeRoute();
@@ -24,7 +7,6 @@ function initURLListener(){
 
 $(document).ready(function() {
 
-    // changePageContent("homeContent");
-
     initURLListener();
+    
 })
--- final-project/data/data.js
+++ final-project/data/data.js
@@ -1,21 +1,70 @@
 export function changeRoute(){
     let hashTag = window.location.hash;
     let pageID = hashTag.replace("#", "");
-    console.log(hashTag + ' ' + pageID);
+
+    // function urlCheck (){
+    //     if(window.location.pathname=="#home"){
+    //         console.log("Home")
+    //     } else {
+    //         console.log("Not Home")
+    //     }
+    // }
 
     if(pageID != ""){
+
         $.get(`pages/${pageID}.html`, function (data) {
-            // console.log("data " + data);
+
+            // $(".navigation").remove();
+            // $(".navigation2").append();
+            $(".navigationApp").append(<div class="navigation">
+            <nav>
+              <a href="#home" class="travelFly"
+                ><img src="images/logo/logo-white.png" alt=""
+              /></a>
+              <a href="#home">home</a>
+              <h2>|</h2>
+              <a href="#about">about</a>
+              <h2>|</h2>
+              <a href="#tours">tours</a>
+              <h2>|</h2>
+              <a href="#specialOffers">special offers</a>
+              <h2>|</h2>
+              <a href="#blog">blog</a>
+              <h2>|</h2>
+              <a href="#contact" class="lastLink">contact</a>
+              <ul>
+                <i class="fa-solid fa-bars"></i>
+                <li><a href="#home">home</a></li>
+                <li><a href="#about">about</a></li>
+                <li><a href="#tours">tours</a></li>
+                <li><a href="#specialOffers">special offers</a></li>
+                <li><a href="#blog">blog</a></li>
+                <li><a href="#contact">contact</a></li>
+              </ul>
+            </nav>
+            <div class="heroCaption">
+              <h1>travel-fly</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. <br />
+                Dolorum perferendis provident commodi. Iusto, dicta.
+              </p>
+              <div class="readMore"><a href="#about">READ MORE</a></div>
+            </div>
+          </div>)
+
             $("#app").html(data)
 
         })
-        // console.log(pageID)
 
     } else {
         $.get(`pages/home.html`, function (data) {
-            // console.log("data " + data);
-            $("#app").html(data)
+
+            // $(".navigation2").remove();
+            // $(".navigation").append();
+            // urlCheck();
+
+            $("#app").html(data);
         })
-        // console.log("home")
     }
 }
+
